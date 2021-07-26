bd4d8664d423a337ab77a54598d4c0c982967da2
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-07-26 19:00:01 +0500

    Create Gatsby  CSS.js

---
+++ GatsBy/Gatsby  CSS.js
@@ -0,0 +1,15 @@
+// Gatsby INLINE CSS Example
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <Layout>
+       <h1 style={{'color: red'}}>   
+          hello world
+       </h1>
+    </Layout>
+  )
+}
+
+export default App
