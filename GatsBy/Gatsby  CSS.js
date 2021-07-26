968206e7c2c9320dabb203894dbdd1ca73fb9e91
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-07-26 19:00:47 +0500

    Update Gatsby  CSS.js

--- GatsBy/Gatsby  CSS.js
+++ GatsBy/Gatsby  CSS.js
@@ -5,7 +5,7 @@ import React from 'react';
 const App = () => {
   return (
     <Layout>
-       <h1 style={{'color: red'}}>   
+       <h1 style={{color: 'red', background: 'blue'}}>   
           hello world
        </h1>
     </Layout>
