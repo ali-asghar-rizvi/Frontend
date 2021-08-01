cbd9db7a399434f3aaba998706efa2c158f1275f
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-01 23:02:42 +0500

    Update blog.js

--- GatsBy/Gatsby-Project/Project-1/src/pages/blog.js
+++ GatsBy/Gatsby-Project/Project-1/src/pages/blog.js
@@ -1,10 +1,105 @@
 import React from 'react'
+import { FiVideo } from "react-icons/fi";
 import Layout from '../components/common/layout'
 
 const blog = () => {
     return (
         <Layout>
-            <h1>HEllo blog</h1>
+            <section class="blogs">
+                <div class="container">
+                    <div class="col-md-12">
+                        <h1>Blogs</h1>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+
+                        <div class="col-md-4">
+                            <div className="blog-content">
+                                <i><FiVideo /></i>
+                                <h5>Lorem ipsum dolor</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus,
+                                    luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </section>
         </Layout>
     )
 }
