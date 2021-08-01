05482a2c5e42d19f95ee31de7f544efcd4c36f85
Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-01 23:03:06 +0500

    Update about.js

--- GatsBy/Gatsby-Project/Project-1/src/pages/about.js
+++ GatsBy/Gatsby-Project/Project-1/src/pages/about.js
@@ -4,9 +4,26 @@ import Layout from '../components/common/layout'
 const About = () => {
     return (
         <Layout>
-            <h1>HEllo About</h1>
+            <section class="about">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="about-content">
+                                <h1>Who we are</h1>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                    galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                                    into electronic typesetting,</p>
+                                <p> remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+                                    containing Lorem Ipsum passages, and more recently
+                                    with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+</section>
         </Layout>
-    )
+            )
 }
 
-export default About
+            export default About
