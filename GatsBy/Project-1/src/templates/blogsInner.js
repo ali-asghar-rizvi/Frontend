541037d8a8b07cc2ee18044be9fc87ba16adb0e5
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-06 03:16:50 +0500

    Add Templates

---
+++ GatsBy/Project-1/src/templates/blogsInner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Layout from './../components/common/layout';
+const blogTemplates = ({pageContext: {data}}) => {
+    console.log('props',data)
+    return (
+        <Layout>
+            <section style={{display:'flex',justifyContent:'center',flexFlow:'column',alignItems:'center'}}>
+                <h2>{data.title}</h2>
+                <img src={data.image.fluid.src} alt={data.title}/>
+                <p>{data.info.info}</p>
+                <p>{data.price}</p>
+            </section>
+        </Layout>
+    )
+}
+
+export default blogTemplates
