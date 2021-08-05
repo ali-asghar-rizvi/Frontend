df071f900b2836441aaadbcfde2174f09fa2da2f
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-06 03:16:01 +0500

    Upload File

---
+++ GatsBy/Project-1/gatsby-node.js
@@ -0,0 +1,32 @@
+const path = require('path');
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions;
+    const result = await graphql(`
+    {
+        blogs: allContentfulBlogs {
+        nodes {
+          slug
+          info{
+              info
+          }
+          title
+          image{
+              fluid{
+                  src
+              }
+          }
+        }
+      }
+    }
+`)
+    result.data.blogs.nodes.forEach((data) => {
+        createPage({
+            path: `/blog/${data.slug}`,
+            component: path.resolve('./src/templates/blogsInner.js'),
+            context: {
+                data: data
+            }
+        })
+    })
+
+}
