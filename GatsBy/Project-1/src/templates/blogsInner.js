4f76722bea3ef9e1bf02d98619eb33353af73ed3
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-07 05:58:29 +0500

    Update blogsInner.js

--- GatsBy/Project-1/src/templates/blogsInner.js
+++ GatsBy/Project-1/src/templates/blogsInner.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Link } from "gatsby"
 import Layout from './../components/common/layout';
-const blogTemplates = ({pageContext: {data}}) => {
-    console.log('props',data)
+const blogTemplates = ({ pageContext: { data } }) => {
+    console.log('props', data)
     return (
         <Layout>
-            <section style={{display:'flex',justifyContent:'center',flexFlow:'column',alignItems:'center'}}>
-                <h2>{data.title}</h2>
-                <img src={data.image.fluid.src} alt={data.title}/>
-                <p>{data.info.info}</p>
-                <p>{data.price}</p>
+            <section className="Blog-Inner-Pages" >
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 mx-auto">
+                            <h2>{data.title}</h2>
+                            <img src={data.image.fluid.src} alt={data.title} />
+                            <p>{data.info.info}</p>
+                            <p>{data.info.info}</p>
+                            <p>{data.info.info}</p>
+                            <div className="back-to-home">
+                             <Link to="/blog">Back To Blog</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </Layout>
     )
