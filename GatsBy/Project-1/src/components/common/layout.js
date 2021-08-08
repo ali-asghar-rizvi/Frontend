f71b1fbd48f4825b65746aa01ea3fa6613edd46b
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-08 10:00:03 -0700

    Update layout.js

--- GatsBy/Project-1/src/components/common/layout.js
+++ GatsBy/Project-1/src/components/common/layout.js
@@ -4,6 +4,8 @@ import Footer from "./footer";
 import '../../css/style.css'
 import '../../css/responsive.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 const Layout = ({ children }) => {
