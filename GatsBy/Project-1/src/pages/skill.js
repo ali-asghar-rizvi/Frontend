2e87e78343693d4af288c75f0e25e161915b3494
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-08 10:02:31 -0700

    Create skill.js

---
+++ GatsBy/Project-1/src/pages/skill.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/common/layout'
+
+const Skills = () => {
+    return (
+        <Layout>
+            <h1>HEllo Skills</h1>
+        </Layout>
+    )
+}
+
+export default Skills
