d98bb8f32dc1410ddbf670de0a37044f36b84703
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-18 23:23:07 +0500

    Update myReducer.js

--- React/React-Examples/useReducer/Even Odd Example/myReducer.js
+++ React/React-Examples/useReducer/Even Odd Example/myReducer.js
@@ -1,13 +1,25 @@
 const Reducer = (state, action) => {
-  switch (action.type) {
-    case 'check':
-      if ((state.count+1 ) % 2 ===  0)
-      return {count : state.count + 1, oddEven: "Even"};
-      else  return {count : state.count + 1, oddEven: "Odd"};
-    default:
-      return new Error()
-  }
-}
+    switch (action.type) {
+        case 'check':
+            if ((state.count + 1) % 2 === 0)
+                return {
+                    count: state.count + 1, oddEven: "Even"
+                };
+            else
+                return {
+                    count: state.count + 1, oddEven: "Odd"
+                };
+        default:
+            return new Error()
+    }
+};
 
-export Default Reducer;
+export default Reducer;
+
+
+
+
+
+
+
 
