078e5c66a49824ba20321585142aae6b9446deb8
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-18 23:22:51 +0500

    Update App.js

--- React/React-Examples/useReducer/Even Odd Example/App.js
+++ React/React-Examples/useReducer/Even Odd Example/App.js
@@ -1,14 +1,14 @@
-import React, {useReducer} from 'react'
-import Reducer from 'myReducer'
+import React, { useReducer } from 'react'
+import Reducer from './myReducer'
 
 
-function App ()  {
-  const [state, dispatch] = useReducer (Reducer, {count: 1 , oddEven: "Odd"});        // Or Intial State //
+function App() {
+  const [state, dispatch] = useReducer(Reducer, { count: 1, oddEven: "Odd" });        // Or Intial State //
   return (
     <div>
-        <h2> {state.count} </h2>
-        <h2> {state.oddEven} </h2>
-        <button onClick={ () => dispatch( { type: "check" } )}> Check Count </button>
+      <h2> {state.count} </h2>
+      <h2> {state.oddEven} </h2>
+      <button onClick={() => dispatch({ type: "check" })}> Check Count </button>
     </div>
   )
 }
