38b7e650cab2bf909a233e33a2b107029ed29e4e
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-18 23:37:59 +0500

    Create myReducer.js

---
+++ React/React-Examples/useReducer/Even Odd Example 1/myReducer.js
@@ -0,0 +1,19 @@
+const Reducer = (state, action) => {
+    switch (action.type) {
+        case 'check':
+            if ((state.count + 1) % 2 === 0)
+                return {
+                    count: state.count + 1, oddEven: "Even"
+                };
+            else
+                return {
+                    count: state.count + 1, oddEven: "Odd"
+                };
+        default:
+            return new Error()
+    }
+};
+
+export default Reducer;
+
+
