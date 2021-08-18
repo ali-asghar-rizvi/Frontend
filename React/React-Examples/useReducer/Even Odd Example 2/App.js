94dc6069a182562eb37baf680264cba1bd61a569
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-18 23:34:43 +0500

    Update App.js
    In this Example, We Have 2 actions One is Check Action and the second is Reset Action.

--- React/React-Examples/useReducer/Even Odd Example 2/App.js
+++ React/React-Examples/useReducer/Even Odd Example 2/App.js
@@ -1,1 +1,18 @@
+import React, { useReducer } from 'react'
+import Reducer from './myReducer'
 
+
+function App() {
+  const [state, dispatch] = useReducer(Reducer, { count: 1, oddEven: "Odd" });        // Or Intial State //
+  return (
+    <div>
+      <h2> {state.count} </h2>
+      <h2> {state.oddEven} </h2>
+      <button onClick={() => dispatch({ type: "check" })}> Check Count </button>
+      <button onClick={() => dispatch({ type: "reset" })}> Reset Count </button>
+
+    </div>
+  )
+}
+
+export default App
