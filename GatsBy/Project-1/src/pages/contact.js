393692859d1db91bacab5508ddea698d5caf8fd7
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-19 23:59:50 +0500

    Update contact.js

--- GatsBy/Project-1/src/pages/contact.js
+++ GatsBy/Project-1/src/pages/contact.js
@@ -13,6 +13,7 @@ function Contact() {
 
     const FormSubmit = () => {
         alert(`Hello ${data.fullname}, Your Form SuccessFully Submit`)
+        console.log(data);
     }
 
     const changeHandler = (event) => {
@@ -28,7 +29,6 @@ function Contact() {
     }
 
 
-
     return (
         <React.Fragment>
             <Layout>
@@ -38,8 +38,9 @@ function Contact() {
                         <div className="row">
                             <div className="col-md-7 contact-content">
                                 <h4>CONTACT</h4>
-                                <h1>GET IN TOUCH WITH US</h1>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
+                                <h1>SEND US MESSAGE</h1>
+                                <p>If you have any questions,
+                                please click and fill out the form, and we wil get back to you ASAP.</p>
                             </div>
 
 
