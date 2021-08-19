bcbd65e98a91d79190c047118b11000c34d532d8
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-20 00:01:30 +0500

    Update index.js

--- GatsBy/Project-1/src/pages/index.js
+++ GatsBy/Project-1/src/pages/index.js
@@ -1,27 +1,14 @@
 import React from 'react'
+import Home from './home'
 import Layout from '../components/common/layout'
 
-const Home = () => {
-  return (
-    <Layout>
-        <section>
-          <div className="container">
-             <div className="row">
-                 <div className="col-md-8">
-                      <h1>WE DEVELOP UNIQUE DIGITAL EXPERIENCES THAT ARE BOTH ENGAGING AND CREATIVE.</h1>
-                      <h2>The art of combining exceptional design with perfect execution.</h2>
-                   
-                 </div>
-             </div>
-          </div>
-        </section>
-    </Layout>
-  )
+const App = () => {
+    return (
+        <Layout>
+            <Home />
+        </Layout>
+    )
 }
 
-export default Home
-
-
-
-
+export default App
 
