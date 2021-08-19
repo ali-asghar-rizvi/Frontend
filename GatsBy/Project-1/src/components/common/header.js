1e76c2ff75f3dc795957e5aecda57bd43b92a901
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-19 23:31:01 +0500

    Update header.js

--- GatsBy/Project-1/src/components/common/header.js
+++ GatsBy/Project-1/src/components/common/header.js
@@ -1,6 +1,84 @@
+// import React from 'react'
+// import { Link } from 'gatsby'
+// import LOGO from '../../images/logo.png'
+
+
+// const Navbar = () => {
+//     return (
+//         <header>
+//             <div className="container">
+//                 <div className="row Website-Header">
+//                     <div className="col-md-3">
+//                         <div className="Website-logo">
+//                             <figure>
+//                                 <img src={LOGO} className="img-fluid" alt="BestBuy Speciality Website Logo" />
+//                             </figure>
+//                         </div>
+//                     </div>
+//                     <div className="col-md-9">
+//                         <ul className="Website-Navbar">
+//                             <li><Link to="/" activeClassName="active-page">Home</Link></li>                   
+//                             <li><Link to="/about" activeClassName="active-page">About</Link></li>
+//                             <li><Link to="/portfolio" activeClassName="active-page">Portfolio</Link></li>
+//                             <li><Link to="/skill" activeClassName="active-page">Skills</Link></li>
+//                             <li><Link to="/service" activeClassName="active-page">Service</Link></li>
+//                             <li><Link to="/blog" activeClassName="active-page">Blog</Link></li>
+//                             <li><Link to="/contact" activeClassName="active-page">Contact</Link></li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//         </header>
+//     )
+// }
+
+// export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import React from 'react'
 import { Link } from 'gatsby'
-import LOGO from '../../images/logo.png'
+
 
 
 const Navbar = () => {
@@ -8,19 +86,17 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <div className="row Website-Header">
-                    <div className="col-md-3">
+                    <div className="col-md-5">
                         <div className="Website-logo">
-                            <figure>
-                                <img src={LOGO} className="img-fluid" alt="BestBuy Speciality Website Logo" />
-                            </figure>
+                           <h2>Muhammad Ahsan</h2>
                         </div>
                     </div>
-                    <div className="col-md-9">
+                    <div className="col-md-7">
                         <ul className="Website-Navbar">
                             <li><Link to="/" activeClassName="active-page">Home</Link></li>                   
                             <li><Link to="/about" activeClassName="active-page">About</Link></li>
                             <li><Link to="/portfolio" activeClassName="active-page">Portfolio</Link></li>
-                            <li><Link to="/skill" activeClassName="active-page">Skills</Link></li>
+                            {/* <li><Link to="/skill" activeClassName="active-page">Skills</Link></li> */}
                             <li><Link to="/service" activeClassName="active-page">Service</Link></li>
                             <li><Link to="/blog" activeClassName="active-page">Blog</Link></li>
                             <li><Link to="/contact" activeClassName="active-page">Contact</Link></li>
@@ -33,5 +109,32 @@ const Navbar = () => {
 }
 
 export default Navbar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
