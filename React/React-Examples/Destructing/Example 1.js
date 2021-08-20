e0579e0dd42f6a78f61bbbaf8cb726426d34abe7
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-21 02:49:03 +0500

    Create Example 1.js

---
+++ React/React-Examples/Destructing/Example 1.js
@@ -0,0 +1,11 @@
+const myArray = ['Ahsan', 'Shaikh', 1]
+
+const [firstName, lastName, degrees] = myArray
+
+console.log(firstName)
+console.log(lastName)
+console.log(degrees)
+
+const sentence = `${firstName} ${lastName} has ${degrees} Bachlor degrees.`
+
+console.log(sentence)
