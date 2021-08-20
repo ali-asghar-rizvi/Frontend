df2221b218ae009fc5dbc55431944fe99a16af79
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-21 01:43:24 +0500

    Update App.js

--- React/React-Examples/DotFilter Example/App.js
+++ React/React-Examples/DotFilter Example/App.js
@@ -1,1 +1,20 @@
+import React,{Component} from 'react';
+
+class MyFirstReactComponent extends Component {
+   render() {
+       const cityArray = ["Karachi", "Lahore", "Peshawer", "Quetta"]
+
+       return (
+           <ul>
+               {
+                   cityArray.map((city) => <li key={city}> {city} </li>)
+               }
+           </ul>
+       )}
+}
+
+
+export default MyFirstReactComponent;
+
+
 
