c87d58929597401f995298459fca8b0e59912b89
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-26 05:06:22 +0500

    Update about.js

--- GatsBy/Project-1/src/pages/about.js
+++ GatsBy/Project-1/src/pages/about.js
@@ -30,27 +30,20 @@ const About = () => {
 
 
 
-
-
-
-
-
             <section className="about">
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12" >
                             <h1 className="what-we-do">" What We Do </h1>
                         </div>
-                        <div className="col-md-6">
+                        <div className="col-md-6 align-self-center">
                             <div className="about-content">
                                 <h1>We create custom website, Web apps and software that delivers on your business goals</h1>
-                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-                                    galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
-                                    into electronic typesetting.</p>
-                                 <p>Remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
-                                    containing Lorem Ipsum passages, and more recently
-                                    with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                                galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                                into electronic typesetting.</p>
+                               
                             </div>
                         </div>
 
