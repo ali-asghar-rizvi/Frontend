39601df0eb7d7015b4ab61aa863f2b852a06b3da
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-26 05:05:45 +0500

    Update index.js

--- GatsBy/Project-1/src/pages/index.js
+++ GatsBy/Project-1/src/pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Home from './home'
+import Home from '../components/functional/home-banner'
 import Layout from '../components/common/layout'
+import  Aboutmyself  from '../components/functional/aboutmyself'
 
 const App = () => {
     return (
         <Layout>
             <Home />
+            <Aboutmyself />
         </Layout>
     )
 }
