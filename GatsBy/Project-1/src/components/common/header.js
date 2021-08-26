bf52ce38f414b47b9de1c466bd441dc140d05e89
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-08-26 05:04:14 +0500

    Update header.js

--- GatsBy/Project-1/src/components/common/header.js
+++ GatsBy/Project-1/src/components/common/header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { FiPhone} from "react-icons/fi";
+import { FaEnvelope ,  FaFacebook} from "react-icons/fa";
+
 
 
 
@@ -8,22 +11,31 @@ const Navbar = () => {
         <header>
             <div className="container">
                 <div className="row Website-Header">
-                    <div className="col-md-5">
-                        <div className="Website-logo">
-                           <h2>Muhammad Ahsan</h2>
+                    <div className="col-md-3">
+                        <div className="Website-left">
+                         <a href="tel:0300-9269560"> <span><FiPhone /></span> +92 300 926-9560 </a>
                         </div>
                     </div>
-                    <div className="col-md-7">
+                    <div className="col-md-6 mx-auto">
                         <ul className="Website-Navbar">
                             <li><Link to="/" activeClassName="active-page">Home</Link></li>                   
-                            <li><Link to="/about" activeClassName="active-page">About</Link></li>
-                            <li><Link to="/portfolio" activeClassName="active-page">Portfolio</Link></li>
-                            {/* <li><Link to="/skill" activeClassName="active-page">Skills</Link></li> */}
-                            <li><Link to="/service" activeClassName="active-page">Service</Link></li>
                             <li><Link to="/blog" activeClassName="active-page">Blog</Link></li>
+                            <li><Link to="/service" activeClassName="active-page">Service</Link></li>
+                            <li><Link to="#skill-section" activeClassName="active-page">Skills</Link></li>
+                            <li><Link to="/portfolio" activeClassName="active-page">Portfolio</Link></li>
+                            <li><Link to="/about" activeClassName="active-page">Overview</Link></li>
                             <li><Link to="/contact" activeClassName="active-page">Contact</Link></li>
                         </ul>
                     </div>
+
+
+                    <div className="col-md-3">
+                         <div className="Website-right">
+                            <a href="https://www.facebook.com/">  <span> <FaFacebook/>  </span>&nbsp;  </a>
+                            <a href="mailto:ahsan_wins@outlook.com"> <span><FaEnvelope />  </span> </a> 
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </header>
