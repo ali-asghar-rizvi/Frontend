7d71597f53aaa00532069d6715d38465e92ef61c
Muhammad Ahsan Sabir Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-09-27 22:56:25 +0500

    Add files via upload

---
+++ GatsBy/Portfolio/gatsby-config.js
@@ -0,0 +1,16 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.GATSBY_CONTENTFUL_ID ,
+        accessToken:  process.env.GATSBY_CONTENTFUL_API,
+      },
+    },
+  ],
+}
