ed3e0e6356b95f292157811bddbcd17353a4dfa6
Muhammad Ahsan  Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2021-12-03 04:56:50 +0500

    Update index.php

--- Html/thankyou-page/index.php
+++ Html/thankyou-page/index.php
@@ -1,1 +1,65 @@
 
+<?php include 'header.php'; ?>
+
+<style>
+    .continue-shoping-btn {
+        margin-top: 50px;
+    }
+
+    .continue-shoping-btn a {
+
+        color: #209e20;
+        text-decoration: underline;
+
+    }
+
+    .coorect-icon {
+        color: #209e20;
+        padding: 5px 0 25px;
+        display: flex;
+        justify-content: center;
+    }
+
+    .thank-you-heading {
+        font-size: 60px;
+    }
+</style>
+
+
+
+<section style="background-color: #072064;padding: 77px 0;"></section>
+
+<section class="inerservice-banner portbanner" style="padding: 100px 0;">
+    <div class="container">
+        <div class="row">
+
+
+            <div class="col-md-10 mx-auto text-center">
+
+
+                <h1 class="thank-you-heading">Thank You !</h1>
+                <div class="coorect-icon ">
+                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                    <lottie-player src="https://assets4.lottiefiles.com/datafiles/8UjWgBkqvEF5jNoFcXV4sdJ6PXpS6DwF7cK4tzpi/Check Mark Success/Check Mark Success Data.json" background="transparent" speed="0.4" style="width: 300px; height: 300px;" autoplay></lottie-player>
+                </div>
+                <h3> Vendor  registration was completed successfully</h1>
+
+                    <div class="continue-shoping-btn">
+                        <a href="index.php">Get back to our homepage </a>
+                    </div>
+            </div>
+
+
+
+
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
