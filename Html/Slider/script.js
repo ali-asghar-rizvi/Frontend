9d1afe80d174c3837d01a48a02f244591f04d4c7
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-07-16 02:04:02 +0500

    Update script.js

--- Html/Slider/script.js
+++ Html/Slider/script.js
@@ -4,6 +4,9 @@ $('.owl-carousel').owlCarousel({
     nav:false,
     animateIn: 'fadeIn', // add this if need only
     animateOut: 'fadeOut', // and this if need only
+       autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: false,
     smartSpeed: 2000,    // if need only
     responsive:{
         0:{
