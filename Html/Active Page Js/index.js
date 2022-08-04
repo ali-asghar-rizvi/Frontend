3e1f1702036e5aa56c67e4bf199189b6665bbf5d
M. Ahsan Shaikh <55804800+Ahsan2001@users.noreply.github.com>
2022-08-04 05:39:31 +0500

    New script

--- Html/Active Page Js/index.js
+++ Html/Active Page Js/index.js
@@ -1,1 +1,12 @@
 
+
+$(function () {
+    let url = window.location.href.split('/')
+    $.each($('.main-menu a'), function () {
+        if (url[url.length - 1] === $(this).attr('href')) {
+            $('.main-menu a').removeClass("active");
+            $(this).addClass("active");
+        }
+    });
+});
+
