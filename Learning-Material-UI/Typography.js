bcad74e06e6fa3b35cb088b3842f24bf1cf79bcf
M. Ahsan Shaikh <info@ahsanshaikh.com>
2022-08-25 00:45:40 +0500

    Update Typography.js

--- Learning-Material-UI/Typography.js
+++ Learning-Material-UI/Typography.js
@@ -1,1 +1,44 @@
+import * as React from 'react';
+import { Typography } from '@mui/material';
 
+function App() {
+  return <>
+    <Typography variant="h1" gutterBottom>
+      h1. Heading
+    </Typography>
+    <Typography variant="h2" gutterBottom>
+      h2. Heading
+    </Typography>
+    <Typography variant="h3" gutterBottom>
+      h3. Heading
+    </Typography>
+    <Typography variant="h4" gutterBottom>
+      h4. Heading
+    </Typography>
+    <Typography variant="h5" gutterBottom>
+      h5. Heading
+    </Typography>
+    <Typography variant="h6" gutterBottom>
+      h6. Heading
+    </Typography>
+
+    <Typography variant="body1" gutterBottom color="primary">
+      lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+
+    <Typography variant="body2" gutterBottom color="secondary">
+      lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+  
+    <Typography variant="subtitle1" gutterBottom align="center">
+      lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+
+    <Typography variant="subtitle2" gutterBottom align="justify">
+      lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </Typography>
+    
+  </>;
+}
+
+export default App;
