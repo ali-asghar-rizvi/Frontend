0d5194c1b5565209e7262b630efe30c5acca6935
M. Ahsan Shaikh <info@ahsanshaikh.com>
2022-09-15 03:35:06 +0500

    Update Layout.js

--- React/Layout.js
+++ React/Layout.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Footer, Header } from '../components'
-import { Outlet } from 'react-router-dom';
 
 const Layout = (props) => {
   return (
    <>
     <Header />
-    {/* Tridition way  
        <div className="content">
             {props.children}
         </div> 
-     */}
-      <Outlet />
     <Footer />
    </>
   )
