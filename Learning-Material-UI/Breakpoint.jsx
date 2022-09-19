0ffb46f07a88d9f940f1e3272e4a8943fb09a4ac
M. Ahsan Shaikh <info@ahsanshaikh.com>
2022-09-20 00:04:57 +0500

    Update Breakpoint.jsx

--- Learning-Material-UI/Breakpoint.jsx
+++ Learning-Material-UI/Breakpoint.jsx
@@ -1,1 +1,17 @@
 
+<Container sx={{
+  display: 'flex', 
+  flexDirection: {xs: 'column', sm: 'row'}}}
+>
+  {...children}
+</Container>
+
+
+
+
+Breakpoints in Material Ui 
+xs, extra-small: 0px
+sm, small: 600px
+md, medium: 900px
+lg, large: 1200px
+xl, extra-large: 1536px
